34eff09a100ad5ca932a957aff72b8a97598dd18
ENDAVA\amujezin <anida.mujezin@endava.com>
2025-04-28 09:33:01 +0200

    changes for confirmation mail

--- controller/appController.js
+++ controller/appController.js
@@ -12,6 +12,8 @@ const sendEmail = async (to, formData) => {
 
     const { Name, PhoneNumber, Email, TypeOfVehicle, CheckBox1, CheckBox2, CheckBox3, NumberOfGuests, CheckIn, CheckOut, Message, TypeOfCamping } = formData;
 
+   
+
     if (!Name || !PhoneNumber || !Email) {
       throw new Error('Name, PhoneNumber, and Email are required fields.');
     }
@@ -133,4 +135,121 @@ const sendMessage = async (to, formData) => {
     throw error;
   }
 };
-module.exports = { sendEmail, sendMessage };
+
+const sendRaftReservation = async (to, formData) => {
+  try {
+    if (!formData || typeof formData !== 'object') {
+      throw new Error('Invalid formData. Expected an object.');
+    }
+
+    const { Name,PhoneNumber,NumberOfGuests, CheckIn, Email,TypeOfRafting, Message } = formData;
+
+    if (!Name || !Email) {
+      throw new Error('Name and Email are required fields.');
+    }
+     // Kreiramo novi objekt sa samo relevantnim poljima
+     const filteredData = {
+      Name: formData.Name,
+      PhoneNumber: formData.PhoneNumber,
+      Email: formData.Email,
+      NumberOfGuests: formData.NumberOfGuests,
+      CheckIn: formData.CheckIn,
+      Message: formData.Message,
+      TypeOfRafting: formData.TypeOfRafting
+    };
+
+    console.log("Nakon filtriranja:", filteredData);
+
+    if (!filteredData.Name || !filteredData.Email) {
+      throw new Error('Name and Email are required fields.');
+    }
+
+    let transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: EMAIL,
+        pass: PASSWORD,
+      },
+    });
+
+    let MailGenerator = new Mailgen({
+      theme: 'default',
+      product: {
+        name: 'Mailgen',
+        link: 'https://mailgen.js/',
+      },
+    });
+
+    let response = {
+      body: {
+        name: 'Camping Neretva',
+        intro: `This is a rafting reservation: `,
+        table: {
+          data: [
+            { item: 'Name', description: Name },
+            { item: 'Phone Number', description: PhoneNumber },
+            { item: 'Email', description: Email },
+            { item: 'Number of Guests', description: NumberOfGuests },
+            { item: 'Check In', description: CheckIn },
+            { item: 'Type of Rafting', description: TypeOfRafting },
+            { item: 'Message', description: Message || 'N/A' }
+          ],
+        },
+        outro: 'Please contact customer in next 24 hours for this message.',
+      },
+    };
+
+    let mail = MailGenerator.generate(response);
+
+    let message = {
+      from: EMAIL,
+      to: to,
+      subject: 'Camping Neretva Rafting Reservation Details',
+      html: mail,
+    };
+
+    await transporter.sendMail(message);
+    console.log('Email sent successfully');
+  } catch (error) {
+    console.error('Error sending email:', error);
+    throw error;
+  }
+};
+
+
+async function sendConfirmationEmail(userEmail, userName) {
+  let transporter = nodemailer.createTransport({
+    service: "gmail", 
+    auth: {
+      user: "neretvac@gmail.com",  
+      pass: "icmlljbvxohqxwut", 
+    },
+  });
+
+  let mailGenerator = new Mailgen({
+      theme: "default",
+      product: {
+          name: "Camping Neretva",
+          link: "https://campingneretva.ba",
+      },
+  });
+
+  let emailBody = mailGenerator.generate({
+      body: {
+          name: userName,
+          intro: "Thanks for making reservation!",
+          outro: " We appreciate your trust in us and look forward to welcoming you soon! If you have any questions in the meantime, feel free to contact us at campingneretva@gmail.com",
+      },
+  });
+
+  let mailOptions = {
+      from: "campingneretva@gmail.com",
+      to: userEmail,
+      subject: "Reservation Confirmation",
+      html: emailBody,
+  };
+
+  await transporter.sendMail(mailOptions);
+}
+
+module.exports = { sendEmail, sendMessage, sendRaftReservation, sendConfirmationEmail};
--- routes/route.js
+++ routes/route.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { sendEmail, sendMessage } = require('../controller/appController.js') // Dodajte ovu liniju
+const { sendEmail, sendMessage, sendRaftReservation, sendConfirmationEmail } = require('../controller/appController.js') // Dodajte ovu liniju
 
 
 router.post('/sendEmail', async (req, res) => {
@@ -8,6 +8,14 @@ router.post('/sendEmail', async (req, res) => {
 
   try {
     await sendEmail(to, formData);
+    await sendConfirmationEmail( 
+      formData.Email, // Email korisnika
+      formData.Name,  // Ime korisnika
+      formData.CheckIn, // Datum prijave
+      formData.CheckOut, // Datum odjave
+      formData.TypeOfCamping, // Tip raftinga (ili kampovanja, zavisno o nazivu)
+      formData.numberOfGuests  
+  );
     res.status(200).json({ message: 'Email sent successfully' });
   } catch (error) {
     console.error('Error sending email:', error);
@@ -26,5 +34,26 @@ router.post('/sendMessage', async (req, res) => {
     res.status(500).json({ error: 'Failed to send message' });
   }
 });
+
+
+router.post('/sendRaftReservation', async (req, res) => {
+  const { to, formData } = req.body;
+
+  try {
+    await sendRaftReservation(to, formData);
+    
+    // Dodajte poziv za slanje potvrde o emailu nakon slanja rafting rezervacije
+    await sendConfirmationEmail(
+      formData.Email, // Email korisnika
+      formData.Name,  // Ime korisnika
+    );
+    
+    res.status(200).json({ message: 'Rafting reservation sent and confirmation email sent successfully' });
+  } catch (error) {
+    console.error('Error sending rafting reservation or confirmation email:', error);
+    res.status(500).json({ error: 'Failed to send rafting reservation or confirmation email' });
+  }
+});
+
 
 module.exports = router;
