f3207200d749abc9a7ec674706d444454cbfce25
ENDAVA\amujezin <anida.mujezin@endava.com>
2025-08-31 16:10:10 +0200

    node v3

--- server.js
+++ server.js
@@ -1,18 +1,7 @@
-// app.js
-const express = require('express');
-const appRoute = require('./routes/route.js');
-const app = express();
-const cors = require("cors");
-
+// server.js
+const app = require('./app');
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
-
-app.use(cors());
-
-/** routes */
-app.use('/api', appRoute);
-
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
